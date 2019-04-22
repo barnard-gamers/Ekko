b4e16bda81b4607e6eade0ace787059d274b83cf
poyo555 <kou555.hoshi@gmail.com>
2019-04-22 14:52:58 +0900

    コード全体の変更

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
 require('dotenv').config()
-let schedule = ["13:25","9:30", "9:45" ,"10:35","10:45", "11:35", "11:45", "12:35", "13:15","14:00", "14:15", "15:00", "15:15", "16:00"]
 var SlackBot = require('slackbots');
+let schedule = {
+  "9:30" : "始業、今日も一日頑張ってください ",
+  "9:45" :  "１限目開始",
+  "10:35":  "１限目休み時間",
+  "10:45":  "２限目開始", 
+  "11:35":  "２限目休み時間", 
+  "11:45":  "3限目レポートの時間", 
+  "12:35":  "昼休み", 
+  "13:15":  "４限目開始 ",
+  "14:00":  "４限目休み時間", 
+  "14:15":  "５限目開始", 
+  "15:00":  "５限目休み時間", 
+  "15:15":  "６限目開始", 
+  "16:00":  "終業、今日も一日お疲れ様でした"
+}
 var bot = new SlackBot({
   token:process.env.SECRET_KEY,
   name:"ekko"
 })
+
 let date = new Date()
 bot.on("start", function(){
   console.log("start")
@@ -13,56 +28,15 @@ bot.on("start", function(){
     let h = date.getHours()
     let m = date.getMinutes()
     let time = h + ":" + m
-    for(var i = 0; i <= schedule.length; i++){
-      if(schedule[i] === time && next_m !== m){
+    Object.keys(schedule).forEach(function(val){
+      //console.log(val)
+      if(val === time && next_m !== m){
         next_m = m
-        switch(schedule[i]){
-          case "9:30":
-            post("始業、今日も一日頑張ってください ")
-            break;
-          case "9:45":
-            post("１限目開始")
-            break;
-          case "10:35":
-            post("１限目休み時間")
-            break;
-          case "10:45":
-            post("２限目開始")
-            break;
-          case"11:35":
-            post("２限目休み時間")
-            break;
-          case"11:45":
-            post("3限目レポートの時間")
-            break;
-          case"12:35":
-            post("昼休み")
-            break;
-          case"13:15":
-            post("４限目開始 ")
-            break;
-          case"14:00":
-            post("４限目休み時間")
-            break;
-          case"14:15":
-            post("5限目開始")
-            break;
-          case "15:00":
-            post("５限目休み時間")
-            break;
-          case"15:15":
-          post("６時間目開始")
-            break;
-          case"16:00":
-            post("終業、今日も一日お疲れ様でした")
-            break;
-        }
-
+        post(schedule[val])
       }
-    }
-  }, 1000);
+    })
+  })
 })
-
 function post(message){
   let params = {
     icon_url: 'https://www.mobafire.com/images/avatars/ekko-classic.png'
