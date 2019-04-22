3282c37ef50f54ddee30352a0090d6d1573ef58b
poyo555 <kou555.hoshi@gmail.com>
2019-04-22 15:25:16 +0900

    date fix

--- App.js
+++ App.js
@@ -9,33 +9,35 @@ let schedule = {
   "11:45":  "3限目レポートの時間", 
   "12:35":  "昼休み", 
   "13:15":  "４限目開始 ",
-  "14:00":  "４限目休み時間", 
+  "14:0":  "４限目休み時間", 
   "14:15":  "５限目開始", 
-  "15:00":  "５限目休み時間", 
+  "15:0":  "５限目休み時間", 
   "15:15":  "６限目開始", 
-  "16:00":  "終業、今日も一日お疲れ様でした"
+  "16:0":  "終業、今日も一日お疲れ様でした"
 }
 var bot = new SlackBot({
   token:process.env.SECRET_KEY,
   name:"ekko"
 })
 
-let date = new Date()
 bot.on("start", function(){
   console.log("start")
   let next_m;
   setInterval(() => {
+    let date = new Date()
     let h = date.getHours()
     let m = date.getMinutes()
     let time = h + ":" + m
     Object.keys(schedule).forEach(function(val){
-      //console.log(val)
       if(val === time && next_m !== m){
         next_m = m
         post(schedule[val])
       }
+      else{
+        console.log(time)
+      }
     })
-  })
+  }, 1000)
 })
 function post(message){
   let params = {
