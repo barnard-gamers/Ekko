6c4e477e99950578913442c6b9cbb9ef3b16cd93
poyo555 <kou555.hoshi@gmail.com>
2019-04-22 13:25:48 +0900

    fix Switch

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-let schedule = ["12:39","9:30", "9:45" ,"10:35","10:45", "11:35", "11:45", "12:35", "13:15","14:00", "14:15", "15:00", "15:15", "16:00"]
+let schedule = ["13:25","9:30", "9:45" ,"10:35","10:45", "11:35", "11:45", "12:35", "13:15","14:00", "14:15", "15:00", "15:15", "16:00"]
 var SlackBot = require('slackbots');
 var bot = new SlackBot({
   token:process.env.SECRET_KEY,
@@ -18,44 +18,44 @@ bot.on("start", function(){
         next_m = m
         switch(schedule[i]){
           case "9:30":
-          post("始業、今日も一日頑張ってください ")
-          break;
+            post("始業、今日も一日頑張ってください ")
+            break;
           case "9:45":
-          post("１限目開始")
-          break;
+            post("１限目開始")
+            break;
           case "10:35":
-          post("１限目休み時間")
-          break;
+            post("１限目休み時間")
+            break;
           case "10:45":
-          post("２限目開始")
-          break;
+            post("２限目開始")
+            break;
           case"11:35":
-          post("２限目休み時間")
-          break;
+            post("２限目休み時間")
+            break;
           case"11:45":
-          post("3限目レポートの時間")
-          break;
+            post("3限目レポートの時間")
+            break;
           case"12:35":
-          post("昼休み")
-          break
+            post("昼休み")
+            break;
           case"13:15":
-          post("４限目開始 ")
-          break;
+            post("４限目開始 ")
+            break;
           case"14:00":
-          post("４限目休み時間")
-          break;
+            post("４限目休み時間")
+            break;
           case"14:15":
-          post("5限目開始")
-          break;
+            post("5限目開始")
+            break;
           case "15:00":
-          post("５限目休み時間")
-          break;
+            post("５限目休み時間")
+            break;
           case"15:15":
           post("６時間目開始")
-          break;
+            break;
           case"16:00":
-          post("終業、今日も一日お疲れ様でした")
-          break;
+            post("終業、今日も一日お疲れ様でした")
+            break;
         }
 
       }
