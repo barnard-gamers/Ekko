8c7ed16b34063375385bcb052a27db3bbadc0e3b
poyo555 <kou555.hoshi@gmail.com>
2019-04-25 14:16:14 +0900

    コードの改善

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
 require('dotenv').config()
 var SlackBot = require('slackbots');
+
 let schedule = {
-  "9:30" : "始業、今日も一日頑張ってください ",
-  "9:45" :  "１限目開始",
-  "10:35":  "１限目休み時間",
-  "10:45":  "２限目開始", 
-  "11:35":  "２限目休み時間", 
-  "11:45":  "3限目レポートの時間", 
-  "12:35":  "昼休み", 
-  "13:15":  "４限目開始 ",
-  "14:0":  "４限目休み時間", 
-  "14:15":  "５限目開始", 
-  "15:0":  "５限目休み時間", 
-  "15:15":  "６限目開始", 
-  "16:0":  "終業、今日も一日お疲れ様でした"
+  "09:30" : "始業、今日も一日頑張ってください ",
+  "09:45" :  "1限目開始",
+  "10:35":  "1限目休み時間",
+  "10:45":  "2限目開始",
+  "11:35":  "2限目休み時間",
+  "11:45":  "3限目レポートの時間",
+  "12:35":  "昼休み",
+  "13:15":  "4限目開始 ",
+  "14:00":  "4限目休み時間",
+  "14:15":  "5限目開始",
+  "15:00":  "5限目休み時間",
+  "15:15":  "6限目開始",
+  "16:00":  "終業、今日も一日お疲れ様でした"
 }
 var bot = new SlackBot({
   token:process.env.SECRET_KEY,
@@ -22,20 +23,17 @@ var bot = new SlackBot({
 
 bot.on("start", function(){
   console.log("start")
-  let next_m;
-  setInterval(() => {
-    let date = new Date()
-    let h = date.getHours()
-    let m = date.getMinutes()
-    let time = h + ":" + m
-    Object.keys(schedule).forEach(function(val){
-      if(val === time && next_m !== m){
-        next_m = m
-        post(schedule[val])
-      }
-    })
-  }, 1000)
 })
+cron.schedule('* * * * *', () => {
+  `${fixTime(new Date().getHours())}:${fixTime(new Date().getMinutes())}` in schedule ? Announce(`${fixTime(new Date().getHours())}:${fixTime(new Date().getMinutes())}`) : null;
+
+  function Announce(time) {
+    post(schedule[time])
+  }
+});
+function fixTime(number) {
+	return (number < 10 ? '0' : '') + number
+}
 function post(message){
   let params = {
     icon_url: 'https://www.mobafire.com/images/avatars/ekko-classic.png'
